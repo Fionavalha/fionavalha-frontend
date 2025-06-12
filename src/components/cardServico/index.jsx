79d694702b4341111a36bdd832d2291acfa6fe4b
Mateus_Ajala <mateuspajala@gmail.com>
2025-06-11 20:03:47 -0400

    Componente_card_servico

--- src/components/cardServico/index.jsx
+++ src/components/cardServico/index.jsx
@@ -1,14 +1,14 @@
 export default function CardServico({ tipoCorte, horario, valor, formaPagamento }) {
   return (
     <>
-      <section className="  w-72 min-h-14 rounded-lg bg-white">
-        <div className="flex justify-between">
-          <p className="font-bold">{tipoCorte}</p>
+      <section className="  w-72 min-h-14 rounded-lg bg-white p-2 ">
+        <div className="flex justify-between items-center">
+          <p className="text-textPrimary textoMedioNegrito">{tipoCorte}</p>
           <p className=" text-textSecond">{horario}</p>
         </div>
-        <div className="flex justify-between ">
+        <div className="flex justify-between items-center">
           <p className="text-textSecond">{formaPagamento}</p>
-          <p className="text-green-600">
+          <p className="text-sucess textoMedioNegrito">
             {valor.toLocaleString("pt-br", {
               style: "currency",
               currency: "BRL",
