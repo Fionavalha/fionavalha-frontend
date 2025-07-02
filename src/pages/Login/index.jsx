4fd6bf0a84eab846b2b1800cd59b47d53633119e
Mateus_Ajala <mateuspajala@gmail.com>
2025-07-01 20:41:47 -0400

    Criação da funcionalidade login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { efetuarLogin } from "../../services/api";
+import { useNavigate } from "react-router";
 
 export default function Login() {
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const navigate = useNavigate()
+
+
+  async function handleEntrar(e) {
+    e.preventDefault()
+    const response = await efetuarLogin(nome, senha);
+    if (response ) {
+     navigate("/inicio")
+    } else {
+      console.log("algo incorreto kkkkk");
+    }
+  }
+
   return (
     <>
       <main className="flex flex-col justify-center items-center bg-ui-background w-screen h-svh">
-        <form action="" className="flex flex-col gap-y-6 items-center px-2 w-full max-w-96 text-white ">
+        <form onSubmit={handleEntrar} className="flex flex-col gap-y-6 items-center px-2 w-full max-w-96 text-white ">
           <h1 className="heading-1">Login</h1>
           <section className="flex flex-col gap-y-3 w-full">
             <div className="flex flex-col">
               <label htmlFor="nome">Nome</label>
-              <input className="input-base h-10" id="nome" type="text" />
+              <input value={nome} onChange={(e) => setNome(e.target.value)} className="input-base h-10" id="nome" type="text" />
             </div>
             <div className="flex flex-col">
               <label htmlFor="senha">Senha</label>
-              <input className="input-base h-10" id="senha" type="password" />
+              <input value={senha} onChange={(e) => setSenha(e.target.value)} className="input-base h-10" id="senha" type="password" />
             </div>
           </section>
           <button className="botao-base w-full h-10 bg-brand-primary">Entrar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.2.103:3000",
+  baseURL: "http://192.168.3.66:3000",
+  auth: {
+    username: "hP3S7l11Kg]",
+    password: "3|^B2hfnI?47",
+  },
 });
+export async function efetuarLogin(pNomeBarbeiro, pSenha) {
+  try {
+    const response = await api.post("/login", {
+      nome_barbeiro: pNomeBarbeiro,
+      senha: pSenha,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
 
 export async function listarCabelos() {
   try {
@@ -10,6 +27,6 @@ export async function listarCabelos() {
     return response;
   } catch (error) {
     console.error(error);
-    return []
+    return [];
   }
 }
