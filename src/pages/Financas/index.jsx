05642ee36f4b365623b322f56dd410b74a2fc720
iranmotaa <iranmota06@gmail.com>
2025-08-08 19:41:30 -0400

    Criação das tabela: Receitas, Despesas, Total. Em finanças

--- src/pages/Financas/index.jsx
+++ src/pages/Financas/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { TabelaDespesas, TabelaReceitas, TabelaTotal } from "./components/Tabela";
+import { useState } from "react";
+import { useEffect } from "react";
+import { consultarDespesas, consultarReceitas } from "../../services/api";
 
 export default function Financas() {
+  const [dadosReceitas, setDadosReceitas] = useState([]);
+  const [totalReceitas, setTotalReceitas] = useState({});
+  const [dadosDespesas, setDadosDespesas] = useState([]);
+  const [totalDespesas, setTotalDespesas] = useState({});
+  const [total, setTotal] = useState(null);
+
+  async function listarReceitas() {
+    const response = await consultarReceitas();
+    setDadosReceitas(response.cortes);
+    setTotalReceitas(response.total_geral);
+  }
+
+  async function listarDespesas() {
+    const response = await consultarDespesas();
+    setDadosDespesas(response.despesas);
+    setTotalDespesas(response.total_geral);
+  }
+
+  useEffect(() => {
+    listarReceitas();
+    listarDespesas();
+  }, []);
+
+  useEffect(() => {
+    const total = Number(totalReceitas.valor_total) - Number(totalDespesas.valor_total);
+    setTotal(total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }));
+  }, [totalReceitas, totalDespesas]);
+
   return (
     <>
-      
+      <section className="flex flex-col justify-center">
+        <TabelaReceitas data={dadosReceitas} total={totalReceitas} />
+        <TabelaDespesas data={dadosDespesas} total={totalDespesas} />
+        <TabelaTotal total={total} />
+      </section>
     </>
-  )
+  );
 }
--- src/utils/formatador.js
+++ src/utils/formatador.js
@@ -1,3 +1,3 @@
 export function formatarDinheiro(valor){
-  return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+  return valor?.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 }
