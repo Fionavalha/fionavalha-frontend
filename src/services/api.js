6bac5cd67ec1b1c90fa1fc08499d3bb86e4742b6
iranmotaa <iranmota06@gmail.com>
2025-11-15 19:56:16 -0400

    Alterações

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://www.fionavalha.com.br/",
+  baseURL: "https://api.fionavalha.com.br/",
 });
 
 api.interceptors.request.use(
